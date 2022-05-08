b7573ffdc7d6ab7ed8f2b593c05a3822e608dd18
Sébastien Gaya <sebastien.gaya@gmail.com>
2022-05-08 18:53:41 +0200

    Api::V1::Credentials for OAuth apps

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users
 
+  namespace :api do
+    namespace :v1 do
+      get '/me' => 'credentials#me'
+    end
+  end
+
   root 'home#index'
 end
